e9c618b35cf88b7c43f61ec168b30434fab833d1
senalIT <110693371+senalIT@users.noreply.github.com>
2023-03-20 11:32:41 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,27 +18,27 @@ global.ANTI_BADWORD = 'true' // true of false
 global.AUTO_REACT = 'false' // true of false
 global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
-global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
+global.alivemsg = 'SENAL-MD ʙᴏᴛ'
 global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
-global.ytname = "YT: Dark" //ur yt chanel name
-global.socialm = "GitHub: Dark" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['0769872326'] //ur owner number
+global.ownername = "Senal" //ur owner name
+global.ytname = "YT: Senal" //ur yt chanel name
+global.socialm = "GitHub: sENALit" //ur github or insta name
+global.location = "Srilanka, Ehaliyagoda, Hewainna" //ur location
 
 //bot bomdy 
 global.session = " " //session Id
-global.ownernomer = "94786802371" //ur number
+global.ownernomer = "94769872326" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://www.darknero.ga/' //script link
+global.premium = ['94769872326'] //ur premium number
+global.botname = ' SENAL- ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
+global.linkz = "https://https://chat.whatsapp.com/HcTbLl8gpTs967fT3aGO38" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
+global.botscript = 'https://https://github.com/senalIT/DARKNERO--V7/' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Dark Nero By" //ur sticker watermark packname
-global.author = "Dark Nero Creating By Cyber Yakuza Team" //ur sticker watermark author
-global.wm = "Dark Bot Inc." //ur watermark
+global.packname = "Senal Bot By" //ur sticker watermark packname
+global.author = "Senal bot Creating By Alone Boy Team" //ur sticker watermark author
+global.wm = "ENAL Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
